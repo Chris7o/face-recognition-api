4650bd22b562d037924edb4cba2fae1d20ed144f
killasusu <christoluka@gmail.com>
2019-01-06 19:42:56 +0530

    adding face-recognition-api

---
+++ facerecognitionapp/src/Components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Rank';
+const Rank = ({name, enteries}) =>{
+return (
+<div>
+<div className='white f3'>
+{`${name}, your current entery count is...`}
+</div>
+<div className='white f1'>
+{enteries}
+</div>
+</div>
+);
+}
+export default Rank;
